9e0d3886a1c078352de222c4e5c44e2fa2258b4d
Ramon96 <rfm1@live.nl>
2020-01-15 19:46:49 +0100

    being able to read out the json file

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,26 @@
 import {
-    select
+    select,
+    dsv,
+    json
 } from 'd3';
   
 
 import * as d3 from 'd3';
 
+const rawdata = require('../input/rawdata.json');
+
+console.log(rawdata[0].response_ID)
+
+animateInsight("value1", 0, 25, 5000);
+animateInsight("value2", 0, 50, 5000);
+
+function cleanData(data){
+    return data.map(d => {
+        return {
+            Respodent: data.response_ID
+        }
+    })
+}
 
 function animateInsight(id, start, end, duration){
     let object = document.getElementById(id);
@@ -30,6 +46,3 @@ function animateInsight(id, start, end, duration){
     Timer = setInterval(run, stepTime);
     run();
 }
-
-animateInsight("value1", 0, 25, 5000);
-animateInsight("value2", 0, 50, 5000);
