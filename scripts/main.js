62284879dd16c89ad723ab8b8464a5b61cc31e46
Ramon96 <rfm1@live.nl>
2020-01-22 23:28:15 +0100

    nested data

--- scripts/main.js
+++ scripts/main.js
@@ -10,13 +10,6 @@ const chartData = cleanChartData(rawdata);
 
 console.log('chartskie: ', chartData);
 
-// const svg = d3.select('#barChart');
-// const width = +svg.attr('width');
-// const height = +svg.attr('height');
-// const margin = { top: 40, right: 30, bottom: 150, left: 100 };
-// const innerWidth = width - margin.left - margin.right;
-// const innerHeight = height - margin.top - margin.bottom;
-// svg.attr('viewBox', [0, 0, width, height]);
 
 const svg = d3.select("#barChart");
 const margin = ({top: 10, right: 10, bottom: 20, left: 40})
@@ -133,104 +126,25 @@ svg.append("g")
     svg.append("g")
     .call(legend);
 
+    
+    
+    //this function needs the html_ID, a starttime, end time, and a duration
+    animateInsight("value1", 0, 25, 5000);
+    animateInsight("value2", 0, 50, 5000);
 
-    function countGroups(data){
+    function restructureData(data){
+        const sortOrigin = d3.nest()
+            .key(d => d.Herkomst)
+            .entries(data)
 
-        data.forEach(d => {
-            origin = d.Herkomst;
-        })
+        const trustByOrigin = d3.nest()
+            .key(d => d.Vetrouwen)
+            .entries(data)
 
-        //kaoloo 
+
     }
 
-/*
-const svg = d3.select('#barChart');
-const width = +svg.attr('width');
-const height = +svg.attr('height');
-const margin = { top: 40, right: 30, bottom: 150, left: 100 };
-const innerWidth = width - margin.left - margin.right;
-const innerHeight = height - margin.top - margin.bottom;
-svg.attr('viewBox', [0, 0, width, height]);
-
-const keys = ["westers", "niet-westers"];
-const groupKey = 'Vetrouwen';
-
-const x0 = d3.scaleBand()
-    .domain(chartData.map(d => d[groupKey]))
-    .rangeRound([0, innerHeight ])
-    .paddingInner(0.1)
-
-    console.log(x0.domain())
-
-// chartData.map(d=> console.log(d))
-// console.log(chartData)
-
-const x1 = d3.scaleBand()
-    .domain(keys)
-    .rangeRound([0, x0.bandwidth()])
-    .padding(0.05)
-    console.log("x1" + x1.domain())
-
-// const y = d3.scaleLinear()
-//     .domain([0, d3.max(chartData, d => d3.max(keys, key => d[key]))]).nice()
-//     .rangeRound([height - margin.bottom, margin.top])
-const color = d3.scaleOrdinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
-
-    // Dit moeten aantal respodenten zijn in %
-const yScale = d3.scaleLinear()
-    .domain([0, 5])
-    // .domain([d3.max(chartData, d => d3.max(keys, key => d[groupKey])), 0]).nice()
-    // .domain([0, d3.max(chartData, d => d3.max(keys, key => d[key]))])
-    .range([0, innerWidth])
-    .nice();
-    
-console.log(yScale.domain())
-
-const g = svg.append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-//append a new group for the y axis and set it on the left side
-g.append('g')
-.style('font-size', '1rem')
-.call(d3.axisLeft(yScale)
-    .tickSize('0'));
-    g.append('g')
-    .style('font-size', '1rem')
-    .call(d3.axisBottom(x0)
-        // .tickSize(-innerHeight)
-        .tickValues([0, 1, 2, 3 ]))
-    .style('stroke-dasharray', ('3, 3'))
-    .attr('transform', `translate(0, ${innerHeight})`)
-    .append('text')
-    .style('font-size', '1rem')
-    .attr('y', 40)
-    .attr('x', innerWidth / 2)
-    .attr('fill', 'white')
-    .text('asfsadf');
-
-    g.append('g')
-    .selectAll('g')
-    .data(chartData)
-    .join('g')
-    .attr('transform', d => `translate(${x0(d[keys])},0)`)
-    // .attr('transform', d => console.log(x0.domain()))
-    .selectAll('rect')
-    .data(d => keys.map(key => ({key, value: d[groupKey]})))
-    .join('rect')
-    .attr('y', d => yScale(d))
-    .attr('x', d => x1(d.key))
-    // .attr('height', x1.bandwidth())
-    .attr('width', x1.bandwidth())
-    .attr('height', d => yScale(d.value) - yScale(0))
-    .attr('fill', d => color(d.key));
-
-    */
-   
-   //this function needs the html_ID, a starttime, end time, and a duration
-   animateInsight("value1", 0, 25, 5000);
-   animateInsight("value2", 0, 50, 5000);
-
+    console.log(restructureData(chartData))
 
 // westers : 1, 6
 // niet westers :  2 ,3 4 ,5 ,7 
