28bd4a4a8cbd15c2c17e00fa877bcf8d03dd6eca
Ramon96 <rfm1@live.nl>
2020-01-22 13:31:38 +0100

    with nicks help less errors

--- scripts/main.js
+++ scripts/main.js
@@ -4,67 +4,58 @@ import {
     json,
     scaleBand
 } from 'd3';
-  
-
 import * as d3 from 'd3';
-
 const rawdata = require('../input/rawdata.json');
 const chartData = cleanChartData(rawdata);
 
-console.log(chartData);
+console.log('chartskie: ', chartData);
 
 const svg = d3.select('#barChart');
-
 const width = +svg.attr('width');
 const height = +svg.attr('height');
-
 const margin = { top: 40, right: 30, bottom: 150, left: 100 };
 const innerWidth = width - margin.left - margin.right;
 const innerHeight = height - margin.top - margin.bottom;
-
 svg.attr('viewBox', [0, 0, width, height]);
-
 const keys = ["westers", "niet-westers"];
 const groupKey = 'Vetrouwen';
-
-
 const x0 = d3.scaleBand()
     .domain(chartData.map(d => d[groupKey]))
     .rangeRound([0, innerHeight ])
     .paddingInner(0.1)
 
-  //  console.log(x0.domain())
-
 const x1 = d3.scaleBand()
     .domain(keys)
     .rangeRound([0, x0.bandwidth()])
     .padding(0.05)
+    console.log(x1.domain())
 
 // const y = d3.scaleLinear()
 //     .domain([0, d3.max(chartData, d => d3.max(keys, key => d[key]))]).nice()
 //     .rangeRound([height - margin.bottom, margin.top])
-
 const color = d3.scaleOrdinal()
-.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
 
+    // Dit moeten aantal respodenten zijn in %
 const yScale = d3.scaleLinear()
     .domain([0, 5]).nice()
+    // .domain([0, d3.max(chartData, d => d3.max(keys, key => d[groupKey]))]).nice()
     .range([0, innerWidth])
     .nice();
-
+    
+console.log(yScale.domain())
 
 const g = svg.append('g')
-.attr('transform', `translate(${margin.left}, ${margin.top})`);
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 //append a new group for the y axis and set it on the left side
 g.append('g')
 .style('font-size', '1rem')
-.call(d3.axisLeft(x0)
+.call(d3.axisLeft(yScale)
     .tickSize('0'));
-
     g.append('g')
     .style('font-size', '1rem')
-    .call(d3.axisBottom(yScale)
+    .call(d3.axisBottom(x0)
         .tickSize(-innerHeight)
         .tickValues([0, 1, 2, 3, 4, 5]))
     .style('stroke-dasharray', ('3, 3'))
@@ -80,55 +71,16 @@ g.append('g')
     .selectAll('g')
     .data(chartData)
     .join('g')
-    .attr('transform', d => `translate(0,${x0(d[groupKey])})`)
+    .attr('transform', d => `translate(${x0(d[keys])},0)`)
     .selectAll('rect')
-    .data(d => keys.map(key => ({key, value: d[key]})))
+    .data(d => keys.map(key => ({key, value: d[groupKey]})))
     .join('rect')
-    .attr('y', d => x1(d.key))
-    .attr('x', d => yScale(d))
+    .attr('y', d => yScale(d))
+    .attr('x', d => x1(d.key))
     // .attr('height', x1.bandwidth())
-    .attr('height', d => console.log(d))
-    .attr('width', d => yScale(d.value) - yScale(0))
+    .attr('width', x1.bandwidth())
+    .attr('height', d => yScale(d.value) - yScale(0))
     .attr('fill', d => color(d.key));
-
-
-// const xAxis = g => g
-//     .attr("transform", `translate(0,${height - margin.bottom})`)
-//     .call(d3.axisBottom(x0).tickSizeOuter(0))
-//     .call(g => g.select(".domain").remove())
-
-// const yAxis = g => g
-// .attr("transform", `translate(${margin.left},0)`)
-// .call(d3.axisLeft(y).ticks(null, "s"))
-// .call(g => g.select(".domain").remove())
-// .call(g => g.select(".tick:last-of-type text").clone()
-//     .attr("x", 3)
-//     .attr("text-anchor", "start")
-//     .attr("font-weight", "bold")
-//     .text(chartData.y))
-
-//     svg.append("g")
-//     .selectAll("g")
-//     .data(chartData)
-//     .join("g")
-//       .attr("transform", d => `translate(${x0(d[groupKey])},0)`)
-//     .selectAll("rect")
-//     .data(d => keys.map(key => ({key, value: d[key]})))
-//     .join("rect")
-//       .attr("x", d => x1(d.key))
-//       .attr("y", d => y(d.value))
-//       .attr("width", x1.bandwidth())
-//       .attr("height", d => y(0) - y(d.value))
-//       .attr("fill", d => color(d.key));
-
-//   svg.append("g")
-//       .call(xAxis);
-
-//   svg.append("g")
-//       .call(yAxis);
-
-//   svg.append("g")
-//       .call(legend);
 
 //this function needs the html_ID, a starttime, end time, and a duration
 animateInsight("value1", 0, 25, 5000);
@@ -137,7 +89,6 @@ animateInsight("value2", 0, 50, 5000);
 // westers : 1, 6
 // niet westers :  2 ,3 4 ,5 ,7 
 // weg : 8
-
 
 // This function checks the origin of the induvidual 1 or 6 is western anything else is non western
 function ancestry(origin){
@@ -160,7 +111,6 @@ function cleanChartData(data){
              Rondkomen: d.stel_rondkomen
         }
     })
-
 }
 
 //this functions handels the countup on the one page
@@ -170,11 +120,9 @@ function animateInsight(id, start, end, duration){
     let minTimer = 50;
     let stepTime = Math.abs(Math.floor(duration/range));
     stepTime = Math.max(stepTime, minTimer);
-
     let startTime = new Date().getTime();
     let endTime = startTime + duration;
     let Timer;
-
     function run(){
         let now = new Date().getTime();
         let remaining = Math.max((endTime - now)/duration, 0);
@@ -184,7 +132,6 @@ function animateInsight(id, start, end, duration){
             clearInterval(Timer);
         }
     }
-
     Timer = setInterval(run, stepTime);
     run();
 }
