dba3cc439cc4adece1b8c268e4cad40e2cfff521
Ramon96 <rfm1@live.nl>
2020-01-23 17:47:57 +0100

    :rat:

--- scripts/main.js
+++ scripts/main.js
@@ -21,57 +21,60 @@ const color = d3.scaleOrdinal()
 function sortNumber(a, b) {
   return a - b;
 }
-const x0 = d3.scaleBand()
+const bar_x0 = d3.scaleBand()
     .domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     .rangeRound([margin.left, width - margin.right])
     .paddingInner(0.1)
 
-const x1 = d3.scaleBand()
+const bar_x1 = d3.scaleBand()
         .domain(keys)
-        .rangeRound([0, x0.bandwidth()])
+        .rangeRound([0, bar_x0.bandwidth()])
         .padding(0.05)
 
-const y = d3.scaleLinear()
-    .domain([0, 100]).nice()
+const y2 = d3.scaleLinear()
+    .domain([0, 50]).nice()
     .rangeRound([height - margin.bottom, margin.top])
 
 const xAxis = g => g
     .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x0).tickSizeOuter(0))
+    .call(d3.axisBottom(bar_x0).tickSizeOuter(0))
     .call(g => g.select(".domain").remove())
 
 const yAxis = g => g
         .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y).ticks(null, "s"))
+        .call(d3.axisLeft(y2).ticks(null, "s"))
         .call(g => g.select(".domain").remove())
         .call(g => g.selectAll(".tick text").clone()
             .attr("text-anchor", "start")
-            .attr("font-weight", "bold"));
+            .attr("font-weight", "bold"))
+
+        
             
     restructureData(chartData)
 
     function update(data){
         const rects = bar.selectAll("rect");
         rects.data(data)
-                .attr("transform", d => `translate(${x0(d[groupKey])},0)`)
-                .attr("x", d => x1(d.herkomst))
-                .attr("width", x1.bandwidth())
+                .attr("transform", d => `translate(${bar_x0(d[groupKey])},0)`)
+                .attr("x", d => bar_x1(d.herkomst))
+                .attr("width", bar_x1.bandwidth())
                 .attr("fill", d => color(d.herkomst))
                 .transition()
                 .delay((d, i) => {return i * 1})
                 .duration(700)
                 .ease(d3.easeQuadOut)
-                .attr("y", d => y(d.percentage))
-                .attr("height", d => y(0) - y(d.percentage));
+                .attr("y", d => y2(d.percentage))
+                .attr("height", d => y2(0) - y2(d.percentage));
+
         rects.data(data)
             .enter().append('rect')  
-            .attr("transform", d => `translate(${x0(d[groupKey])},0)`)
-            .attr("x", d => x1(d.herkomst))
+            .attr("transform", d => `translate(${bar_x0(d[groupKey])},0)`)
+            .attr("x", d => bar_x1(d.herkomst))
             .attr("data-percentage", d => d.percentage)
-            .attr("width", x1.bandwidth())
-            .attr("y", d => y(d.percentage))
-                .attr("height", d => y(0) - y(d.percentage))
-                .attr("fill", d => color(d.herkomst));
+            .attr("width", bar_x1.bandwidth())
+            .attr("y", d => y2(d.percentage))
+                .attr("height", d => y2(0) - y2(d.percentage))
+                .attr("fill", d => color(d.herkomst))
 
         rects.data(data).exit().remove();
 
@@ -93,6 +96,7 @@ const yAxis = g => g
             .call(xAxis);
         svg.append("g")
             .call(yAxis);
+
 }
     const legend = svg => {
         const g = svg
@@ -199,3 +203,216 @@ function animateInsight(id, start, end, duration){
     Timer = setInterval(run, stepTime);
     run();
 }
+
+// dkjfakl;ads
+
+
+
+var svg2 = d3.select(".columnChart"),
+margin2 = {top: 50, right: 50, bottom: 100, left: 100},
+width2 = +svg2.attr("width") - margin2.left - margin2.right,
+height2 = +svg2.attr("height") - margin2.top - margin2.bottom,
+g = svg2.append("g").attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+// The scale spacing the groups:
+var column_x0 = d3.scaleBand()
+.rangeRound([0, width2])
+.paddingInner(0.1);
+
+// The scale for spacing each group's bar:
+var column_x1 = d3.scaleBand()
+.padding(0.05);
+
+var column_y = d3.scaleLinear()
+.rangeRound([height2, 0]);
+
+var z = d3.scaleOrdinal()
+.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+console.log("je moeder")
+
+d3.csv("../input/data.csv", function(d, i, columns) {
+for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = +d[columns[i]];
+return d;
+}).then(function(data) {
+console.log(data);
+
+var keys = data.columns.slice(1);
+
+console.log('keys');
+console.log(keys);
+column_x0.domain(data.map(function(d) { return d.State; }));
+column_x1.domain(keys).rangeRound([0, column_x0.bandwidth()]);
+column_y.domain([0, d3.max(data, function(d) { return d3.max(keys, function(key) { return d[key]; }); })]).nice();
+
+g.append("g")
+    .selectAll("g")
+    .data(data)
+    .enter().append("g")
+    .attr("class","bar")
+    .attr("transform", function(d) { return "translate(" + column_x0(d.State) + ",0)"; })
+    .selectAll("rect")
+    .data(function(d) { return keys.map(function(key) { return {key: key, value: d[key]}; }); })
+    .enter().append("rect")
+    .attr("x", function(d) { return column_x1(d.key); })
+    .attr("y", function(d) { return column_y(d.value); })
+    .attr("width", column_x1.bandwidth())
+    .attr("height", function(d) { return height2 - column_y(d.value); })
+    .attr("fill", function(d) { return z(d.key); });
+
+g.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + height2 + ")")
+    .call(d3.axisBottom(column_x0));
+
+g.append("g")
+    .attr("class", "y axis")
+    .call(d3.axisLeft(column_y).ticks(null, "s"))
+    .append("text")
+    .attr("x", 2)
+    .attr("column_y", column_y(column_y.ticks().pop()) + 0.5)
+    .attr("dy", "0.32em")
+    .attr("fill", "white")
+    .attr("font-weight", "italic")
+    .attr("text-anchor", "start")
+          .style("font-size", "20px")
+          .attr("font-style", "italic")
+          .attr("transform", "translate(-70,230) rotate(-90)")
+    .text("Aantal respondenten in %");
+
+g.append("g")
+    .append("text")
+    .attr("fill", "white")
+    .attr("font-weight", "italic")
+    .attr("text-anchor", "start")
+          .style("font-size", "20px")
+          .attr("font-style", "italic")
+          .attr("transform", "translate(210,320)")
+    .text("\"De politie was beleefd gedurende het contact.\"");
+
+var legend = g.append("g")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", 14)
+    .attr("text-anchor", "end")
+    .attr("fill","white")
+    .selectAll("g")
+    .data(keys.slice().reverse())
+    .enter().append("g")
+    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+legend.append("rect")
+    .attr("x", width2 - 17)
+    .attr("width", 15)
+    .attr("height", 15)
+    .attr("fill", z)
+    .attr("stroke", z)
+    .attr("stroke-width",2)
+    .on("click",function(d) { update(d) });
+
+legend.append("text")
+    .attr("x", width2 - 24)
+    .attr("y", 9.5)
+    .attr("dy", "0.32em")
+    .text(function(d) { return d; });
+
+var filtered = [];
+
+////
+//// Update and transition on click:
+////
+
+function update(d) {
+
+    //
+    // Update the array to filter the chart by:
+    //
+
+    // add the clicked key if not included:
+    if (filtered.indexOf(d) == -1) {
+        filtered.push(d);
+        // if all bars are un-checked, reset:
+        if(filtered.length == keys.length) filtered = [];
+    }
+    // otherwise remove it:
+    else {
+        filtered.splice(filtered.indexOf(d), 1);
+    }
+
+    //
+    // Update the scales for each group(/states)'s items:
+    //
+    var newKeys = [];
+    keys.forEach(function(d) {
+        if (filtered.indexOf(d) == -1 ) {
+            newKeys.push(d);
+        }
+    })
+    column_x1.domain(newKeys).rangeRound([0, column_x0.bandwidth()]);
+    column_y.domain([0, d3.max(data, function(d) { return d3.max(keys, function(key) { if (filtered.indexOf(key) == -1) return d[key]; }); })]).nice();
+
+    // update the column_y axis:
+    svg2.select("y")
+        .transition()
+        .call(d3.axisLeft(column_y).ticks(null, "s"))
+        .duration(500);
+
+
+    //
+    // Filter out the bands that need to be hidden:
+    //
+    var bars = svg2
+    .selectAll(".bar")
+    .selectAll("rect")
+      .data(function(d) { return keys.map(function(key) { return {key: key, value: d[key]}; }); })
+
+    bars.filter(function(d) {
+            return filtered.indexOf(d.key) > -1;
+        })
+        .transition()
+        .attr("x", function(d) {
+            return (+d3.select(this).attr("x")) + (+d3.select(this).attr("width"))/2;
+        })
+        .attr("height",0)
+        .attr("width",0)
+        .attr("y", function(d) { return height2; })
+        .duration(500);
+
+    //
+    // Adjust the remaining bars:
+    //
+    bars.filter(function(d) {
+            return filtered.indexOf(d.key) == -1;
+        })
+        .transition()
+        .attr("x", function(d) { return column_x1(d.key); })
+        .attr("y", function(d) { return column_y(d.value); })
+        .attr("height", function(d) { return height2 - column_y(d.value); })
+        .attr("width", column_x1.bandwidth())
+        .attr("fill", function(d) { return z(d.key); })
+        .duration(500);
+
+
+    // update legend:
+    legend.selectAll("rect")
+        .transition()
+        .attr("fill",function(d) {
+            if (filtered.length) {
+                if (filtered.indexOf(d) == -1) {
+                    return z(d);
+                }
+                else {
+                    return "white";
+                }
+            }
+            else {
+                return z(d);
+            }
+        })
+        .duration(100);
+
+
+}
+
+});
+
+
